d08cef222291204dd727680c41b53873679db019
TimeBotDemo <TimeBotDemo@protonmail.com>
2024-12-01 19:47:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const handleTweet = async () => {
     try {
-        const response = await fetch('http://api.quotable.io/quotes/random');
+        const response = await fetch('https://api.quotable.io/quotes/random');
         const data = await response.json();
         const quote = data.content;
 
